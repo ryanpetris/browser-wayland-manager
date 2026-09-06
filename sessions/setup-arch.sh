279a3fc8639f3b65715a9063c67ff4bf5205f284
Ryan Petris <ryan@petris.net>
2026-09-06 13:36:33 -0700

    Install Xwayland and audio integration for desktop sessions

--- sessions/setup-arch.sh
+++ sessions/setup-arch.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -eu
-pacman -Syu --noconfirm --needed dbus pipewire pipewire-pulse wireplumber util-linux xterm ttf-dejavu coreutils
+pacman -Syu --noconfirm --needed dbus pipewire pipewire-pulse wireplumber gst-plugin-pipewire pipewire-alsa libpulse gst-plugins-ugly xorg-xwayland util-linux xterm ttf-dejavu coreutils
 id -u bw >/dev/null 2>&1 || useradd -m -u 1000 bw
